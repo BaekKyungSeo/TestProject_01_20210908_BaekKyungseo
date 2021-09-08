c1d978aef9f0dcfb6f039be2c5379b46279c022f
ehdrb <ehdrb@192.168.219.104>
2021-09-08 15:22:03 +0900

    testProject_0908

---
+++ src/main/java/com/javasiyeon/ex/TestProject.java
@@ -0,0 +1,70 @@
+package com.javasiyeon.ex;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestProject
+ */
+@WebServlet("/TestProject")
+public class TestProject extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestProject() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// doGet(request, response);
+		System.out.println("doPost 메서드가 실행됨");
+		
+		request.setCharacterEncoding("EUC-KR");
+		
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+		String name = request.getParameter("name");
+		
+		String email_1 = request.getParameter("email_1");
+		String email_2 = request.getParameter("email_2");
+		String job = request.getParameter("job");
+		
+		response.setContentType("text/html; charset=EUC-KR");
+		PrintWriter writer = response.getWriter();
+		
+		writer.println("<html><head></head><body>");
+		writer.println("아이디 : " + id + "<br/>");
+		writer.println("비밀번호 : " + pw + "<br/>");
+		writer.println("이름 : " + name + "<br/>");
+		writer.println("E-mail : " + email_1 + "@ " + email_2 + "<br/> ");
+		writer.println("직업 : " + job + "<br/>");
+
+		writer.println("</body></html>");
+		
+		writer.close();
+		
+		
+	}
+
+}
